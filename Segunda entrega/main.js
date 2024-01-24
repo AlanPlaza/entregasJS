7045ceb146086f0c44d1d29360575839d15716e4
Alan Plaza <aplaza0000@gmail.com>
2024-01-23 21:58:19 -0300

    pruebas

--- Segunda entrega/main.js
+++ Segunda entrega/main.js
@@ -22,7 +22,22 @@ const ingresados = [
 { id: 1, nombre: "Jose", apellido: "Alvarez" },
 { id: 2, nombre: "Nicolas", apellido:"Gimenez" },
 { id: 3, nombre: "Maximiliano" , apellido: "Rodriguez"},
-{ id: 4, nombre: "Lautaro" , apellido: "Lopez"}]
+{ id: 4, nombre: "Lautaro" , apellido: "Lopez"}];
+
+let container = document.getElementById("contenedor")
+container.innerHTML = `<h2>Nombre de alumnos</h2>`
+
+
+for (const ingreso of ingresados) {
+ let contenedor = document.createElement("div");
+ //Definimos el innerHTML del elemento con una plantilla de texto
+
+ contenedor.innerHTML = `
+ <h3> Numero de alumno ${ingreso.id}</h3>
+ <p> Nombre: ${ingreso.nombre}</p>
+ <p> Apellido ${ingreso.apellido}</p>`;
+ document.body.appendChild(contenedor);
+}
 
 let buscadorNombre = prompt("Ingrese nombre del alumno que busca")
 
